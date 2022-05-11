ac936a208a53cb0e3dd9e365c7ebf46f1ae73fc7
Anton Alekseev <toxa23@gmail.com>
2022-05-11 22:38:44 +0300

    new Taquito node url

--- front/src/common/Constants.js
+++ front/src/common/Constants.js
@@ -10,6 +10,6 @@ export const RACE_PATH = '/race';
 
 export const RACE_CONTRACT = 'KT1KsJMkyNxBmb75RF929wTxgpbr65TZT5iK';
 export const UUSD_CONTRACT = 'KT1PiqMJSEsZkFruWMKMpoAmRVumKk9LavX3'; // in testnet
-export const NODE_URL = 'https://hangzhounet.api.tez.ie';
+export const NODE_URL = 'https://hangzhounet.smartpy.io';
 export const NETWORK_TYPE = NetworkType.HANGZHOUNET;
 // export const NODE_URL = 'https://tezos-node.prod.gke.papers.tech'
